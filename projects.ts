46d28bcf0a7a1e637217cb10fecc734c67bfe0aa
oluthje <oluthje@gmail.com>
2024-06-05 11:56:39 -0700

    Added ProjectTemplate component for displaying projects

---
+++ projects.ts
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    "title": "End Reach",
+    "images": [
+      "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1389030/header.jpg?t=1604078934",
+      "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1389030/ss_d6f8ed5207e54fab17ed7f545b1b72d39cf78045.600x338.jpg?t=1604078934",
+      "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1389030/ss_415675f9eef66f977bc31643e9bede0edf5d3c56.600x338.jpg?t=1604078934"
+    ],
+    "githubLink": "https://github.com/oluthje/CaveScroller",
+    "websiteLink": "https://store.steampowered.com/app/1389030/End_Reach/",
+    "description": "A 2D top down shooter game I developed over the summer of 2020. My goal was to develop a game from start to finish in 4 months and publish it on Steam, a goal I achieved through daily dedication to coding, planning, designing and drawing art.",
+    "technologies": [
+      "Godot game engine", "GDScript", "Git"
+    ],
+    "whatILearned": "Developing this game taught me how to stay focused on a single project until completion. I learned to manage a project, push through features, and how to design games.",
+  }
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,132 @@
-// import Link from "next/Link";
+import { projects } from '../../projects';
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      {/* <div className="flex justify-end pt-5 pr-20">
+    <div className="flex flex-col items-center min-h-screen">
+      <div className="flex w-full max-w-2xl m-auto pt-10">
         <Navbar/>
-      </div> */}
-      <main className="flex flex-col items-center justify-between p-24 space-y-2">
-        <div>Hi, I&apos;m Oscar</div>
-        <div>This website is in progress ¯\_(ツ)_/¯</div>
-        <div>Soon it shall contain my all of my awesome projects...</div>
-      </main>
+      </div>
+
+      {/* Introduction */}
+      <div className="flex w-full h-[40rem]">
+        <Introduction />
+      </div>
+
+      {/* My project list */}
+      <div className="flex flex-col space-y-16" >
+        {projects.map((project: any) => <ProjectTemplate {...project} />)}
+      </div>
     </div>
   );
 }
 
-// function Navbar() {
-//   return (
-//     <ul className="flex flex-row space-x-2 list-none">
-//       <li>
-//         <Link href="/">
-//           Home
-//         </Link>
-//       </li>
-//       <li>
-//         <Link href="/">
-//           Projects
-//         </Link>
-//       </li>
-//       <li>
-//         <Link href="/">
-//           About
-//         </Link>
-//       </li>
-//     </ul>
-//   )
-// }
+function Navbar() {
+  return (
+    <div className="flex justify-center justify-between w-full m-auto">
+      <div className="p-4 border border-4 border-blue-300 bg-blue-300" />
+      <ul className="flex items-center justify-center flex-row space-x-10 list-none">
+        <li>
+          <HeaderButton href="/" >Home</HeaderButton>
+        </li>
+        <li>
+          <HeaderButton href="/" >Projects</HeaderButton>
+        </li>
+        <li>
+          <HeaderButton href="/" >About Me</HeaderButton>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+function HeaderButton({ children, href }: { children?: React.ReactNode, href: string }) {
+  return (
+    <a href={href} className="font-bold text-xl">
+      {children}
+    </a>
+  )
+}
+
+function Introduction() {
+  return (
+    <div className="flex justify-center flex-row justify-between w-full max-w-2xl m-auto">
+      <div className="flex flex-col">
+        <div className="font-bold text-5xl">
+          <div>Hello,</div>
+          <div>I'm Oscar Luthje</div>
+        </div>
+        <div className="text-gray-600 text-base w-[15rem]">
+          I'm a software developer and computer science student from UC Santa Cruz.
+        </div>
+      </div>
+      <div>
+        <img className="rounded" src="https://media.licdn.com/dms/image/C5603AQHhGcjpPQyzPg/profile-displayphoto-shrink_200_200/0/1659380116733?e=2147483647&v=beta&t=IVBpEEqSYAKbVHEyVCPjGuDH1RQ_Q3UnBTOY3PetvLI" />
+      </div>
+    </div>
+  )
+}
+
+/**
+ * title
+ * image
+ * github link?
+ * website link?
+ * description
+ * technologies
+ * what I learned
+ */
+
+// !!! add carousel of images
+function ProjectTemplate({
+  title,
+  images,
+  githubLink,
+  websiteLink,
+  description,
+  technologies,
+  whatILearned
+}: {
+  title: string,
+  images?: string[],
+  githubLink?: string,
+  websiteLink?: string,
+  description?: string,
+  technologies?: string[],
+  whatILearned?: string
+}) {
+  return (
+    <div className="flex flex-col max-w-2xl m-auto w-full space-y-4">
+      <div className="font-bold text-5xl">{title}</div>
+      <div className="flex justify-left">
+        {images && images.map(imageSrc =>
+          <img className="bg-red-100 max-h-32 object-contain" src={imageSrc}/>
+        )}
+      </div>
+      <div>
+        {githubLink && <a href={githubLink} className="font-bold text-xl">Github</a>}
+        {websiteLink && <a href={websiteLink} className="font-bold text-xl">Website</a>}
+      </div>
+
+      {/* Description */}
+      <div>
+        <div className="font-bold text-xl">Description</div>
+        <div>{description}</div>
+      </div>
+
+      {/* Technologies */}
+      <div>
+        <div className="font-bold text-xl">Technologies</div>
+        <ul className="list-disc list-inside pl-2">
+          {technologies && technologies.map(tech => <li>{tech}</li>)}
+        </ul>
+      </div>
+
+      {/* What I learned */}
+      {whatILearned && <div>
+        <div className="font-bold text-xl">What I Learned</div>
+        <div>{whatILearned}</div>
+      </div>}
+
+    </div>
+  )
+}
